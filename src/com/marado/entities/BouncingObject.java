6116fae792ef85fe8435be9f27cdf02bbe5fec8c
marado <78569959+Marado-Programer@users.noreply.github.com>
2021-05-11 21:53:31 +0100

    Update BouncingObject.java

--- src/com/marado/entities/BouncingObject.java
+++ src/com/marado/entities/BouncingObject.java
@@ -10,16 +10,17 @@ public class BouncingObject {
     public boolean isBouncing = false; //When the object is bouncing it's true
     
     public double y = 0; //The windowHeight of the object
-    public int objWidth, objHeight = 16;
+    public int objWidth = 16;
+    public int objHeight = 16;
     double initVelocity = 35; //The initial velocity that the object
     double velocity; //The velocity on the air
     double time = 0.001; //Time in the air
-
-    public void render(Graphics g){
-        g.setColor(new Color(0, 255, 0));
-        g.fillRect((App.windowWidth)/2, (App.windowHeight)-((int)y), objWidth, objHeight);
-    }
     
+    public void render(Graphics graphics){
+        graphics.setColor(new Color(0, 255, 0));
+        graphics.fillOval(App.windowWidth/2-objWidth/2, App.windowHeight-(int)y-objHeight, objWidth, objHeight);
+    }
+
     public void bounce(){ //Bounce Action
         if(isBouncing){ //If you clicked <SPACE> now the object it's bouncing
             y = initVelocity*time-0.5*Environment.g*Math.pow(time, 2); //Calculate the object height
